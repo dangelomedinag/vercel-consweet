bc93c9662e171609eb5c0da4666484f454a5c637
dangelo <dangelomedinag@gmail.com>
2021-03-13 19:51:16 -0300

    change 1 from main branch

--- api/date.js
+++ api/date.js
@@ -1,4 +1,5 @@
 // this is a comments update
+// this comment is write from main branch
 module.exports = async (req, res) => {
   res.status(200).send({message:"change from serverless-function branch"});
 };
