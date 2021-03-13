82e80b50d55d036ead373ecbedb0ec020fb6d159
dangelo <dangelomedinag@gmail.com>
2021-03-13 19:18:26 -0300

    update: serverless function <api/Date.js>; remove unnessesary code and comments

--- api/date.js
+++ api/date.js
@@ -1,15 +1,3 @@
-import {db} from "../firestore";
-// let products = 
 module.exports = async (req, res) => {
-
-	// const collection = await db.collection("productos").get()
-
-	
-	// collection.forEach(it=>{
-	// 	products.push(it.data())
-	// })
-
-		
-  const date = new Date().toString();
-  res.status(200).send({Date, message:"change from serverless-function branch"});
+  res.status(200).send({message:"change from serverless-function branch"});
 };
