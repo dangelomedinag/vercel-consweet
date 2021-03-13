93e8fcca329d563564cae8d65fa6399f0be953a8
dangelo <dangelomedinag@gmail.com>
2021-03-13 19:57:59 -0300

    restart state from serverless-function branch

--- api/date.js
+++ api/date.js
@@ -1,6 +1,3 @@
-// this is a comments update
-// this comment is write from main branch
-// this comment is a response to commit to the main branch, from SF branch
 module.exports = async (req, res) => {
   res.status(200).send({message:"change from serverless-function branch"});
 };
